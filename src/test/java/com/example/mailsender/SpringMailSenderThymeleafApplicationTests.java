dc99d7cdb08b794f2aecb92719e2b8078e51c989
Pedro Lacombe <pedrocarlantonio@gmail.com>
2021-09-22 02:25:39 -0300

    First commit

---
+++ src/test/java/com/example/mailsender/SpringMailSenderThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mailsender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMailSenderThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
