63f00764a84bc360b20006497e2930d62c18fb8d
Pedro Lacombe <pedrocarlantonio@gmail.com>
2021-09-22 02:30:46 -0300

    Repository layer for Student model

---
+++ src/main/java/com/example/mailsender/repository/StudentRepository.java
@@ -0,0 +1,10 @@
+package com.example.mailsender.repository;
+
+import com.example.mailsender.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, String> {
+
+}
