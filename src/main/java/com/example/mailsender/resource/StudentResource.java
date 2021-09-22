4a672edfa283f75cc96ce9921670bedb5ec2710e
Pedro Lacombe <pedrocarlantonio@gmail.com>
2021-09-22 02:34:50 -0300

    Student resource, responsible for the Students endpoints

---
+++ src/main/java/com/example/mailsender/resource/StudentResource.java
@@ -0,0 +1,34 @@
+package com.example.mailsender.resource;
+
+import com.example.mailsender.dto.StudentRequestDto;
+import com.example.mailsender.dto.mapper.StudentMapper;
+import com.example.mailsender.model.Student;
+import com.example.mailsender.repository.StudentRepository;
+import com.example.mailsender.service.StudentService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/students")
+public class StudentResource {
+
+    private StudentService studentService;
+    private StudentRepository studentRepository;
+
+    @GetMapping
+    public List<Student> findAll() {
+        return this.studentRepository.findAll();
+    }
+
+    @PostMapping
+    public ResponseEntity<Student> save(@Valid @RequestBody StudentRequestDto studentRequestDto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(this.studentService.save(StudentMapper.fromDto(studentRequestDto)));
+    }
+
+}
